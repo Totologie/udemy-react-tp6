f83fdcb7b90f9ebaf5a21162ef75cea6e06e9e73
Anthony Gourbeyre <anthony.gourbeyre@inforsud.com>
2020-07-06 18:34:30 +0200

    Added Accordion

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Accordion from './components/Accordion'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const items= [
+    {
+        id:"26",
+        title: 'What is React?',
+        content: 'Imeofjezpjigfpozejfzjfoezofjp^'
+    },
+    {
+        id:"34",
+        title: 'What is React?',
+        content: 'Imeofjezpjigfpozejfzjfoezofjp^'
+    },
+    {
+        id:"25286",
+        title: 'What is React?',
+        content: 'Imeofjezpjigfpozejfzjfoezofjp^'
+    }
+]
+
+export default () => {
+    return (
+        <div>
+            <h1>Accordion application</h1>
+            <Accordion items={items} />
+        </div>
+    )
 }
-
-export default App;
